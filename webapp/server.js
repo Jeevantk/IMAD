6fea715d293e1928b3dfc3c9ac271a31e41d4356
jeevantk <jeevan.thomaskoshy@gmail.com>
2017-11-11 12:44:51 +0530

    Todays work

--- webapp/server.js
+++ webapp/server.js
@@ -5,12 +5,74 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+var artticleOne={
+	title: 'Article One | Jeevan Thomas Koshy',
+	heading : 'Article One',
+	date : 'Nov 11 2017',
+	content : `<p>
+					blah blah blah
+				</p>
+				<p>
+					blah blah blah
+				</p>
+				<p>
+					blah blah blah
+				</p>`
+};
+function createTemplate(data){
+	var title=data.title;
+	var date=data.date;
+	var heading=data.heading;
+	var content=data.content;
+	var htmlTemplate=`<html>
+		<head>
+		<title>
+			${title}
+		</title>
+		<meta name="viewport" content="width=device-width initial-scale=1"/>
+		<link href="/ui/style.css" rel="stylesheet" />
+		</head>
+
+		<body>
+			<div class="container">
+				<div>
+					<a href="/"> Home</a>
+				</div>
+				<hr/>
+				<h3>
+					${heading}
+				</h3>
+				<div>
+					${date}
+				</div>
+
+				<div>
+					${content}
+				</div>
+			</div>
+		</body>
+
+	</html>`;
+}
+
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/article-one',function(req,res){
+	res.send(createTemplate(artticleOne))
+});
+
+app.get('/article-two',function(req,res){
+	res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three',function(req,res){
+	res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
